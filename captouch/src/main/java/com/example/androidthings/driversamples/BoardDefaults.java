c55332abbeec29f611f2f06170f6e347a464f628
Dave Smith <smithdave@google.com>
2016-11-28 06:30:27 -0700

    Captouch + GPS Samples
    Driver samples for captouch and gps modules.
    
    Change-Id: I28fc2bb6f52ea06ed2594d4977d40e47ce0a5457
    Signed-off-by: Dave Smith <smithdave@google.com>

---
+++ captouch/src/main/java/com/example/androidthings/driversamples/BoardDefaults.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidthings.driversamples;
+
+import android.os.Build;
+
+@SuppressWarnings("WeakerAccess")
+public class BoardDefaults {
+    private static final String DEVICE_EDISON = "edison";
+    private static final String DEVICE_RPI3 = "rpi3";
+    private static final String DEVICE_NXP = "imx6ul";
+
+    /**
+     * Return the preferred I2C port for each board.
+     */
+    public static String getI2CPort() {
+        // TODO: confirm preferred port for NXP
+        switch (Build.DEVICE) {
+            case DEVICE_EDISON:
+                return "I2C6";
+            case DEVICE_RPI3:
+                return "I2C1";
+            case DEVICE_NXP:
+                return "??";
+            default:
+                throw new IllegalStateException("Unknown Build.DEVICE " + Build.DEVICE);
+        }
+    }
+}
