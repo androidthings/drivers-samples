d4404a65bfa78134d51e8330e0ab429781e61fcc
Johan Euphrosine <proppy@google.com>
2018-02-16 14:49:22 +0900

    drivers-samples/tm1637: update imx pins to match schematics
    tested on imx7 with DP7
    
    Bug: 69023931
    Change-Id: I355c4e4ad44cebc98d6709ab234b64eb66b61eae

--- tm1637/src/main/java/com/example/androidthings/driversamples/BoardDefaults.java
+++ tm1637/src/main/java/com/example/androidthings/driversamples/BoardDefaults.java
@@ -33,9 +33,9 @@ public class BoardDefaults {
             case DEVICE_RPI3:
                 return "BCM20";
             case DEVICE_IMX6UL_PICO:
-                return "GPIO2_IO03";
+                return "GPIO2_IO02";
             case DEVICE_IMX7D_PICO:
-                return "GPIO6_IO14";
+                return "GPIO6_IO15";
             default:
                 throw new IllegalStateException("Unknown Build.DEVICE " + Build.DEVICE);
         }
@@ -49,9 +49,9 @@ public class BoardDefaults {
             case DEVICE_RPI3:
                 return "BCM21";
             case DEVICE_IMX6UL_PICO:
-                return "GPIO4_IO22";
+                return "GPIO2_IO03";
             case DEVICE_IMX7D_PICO:
-                return "GPIO2_IO02";
+                return "GPIO6_IO14";
             default:
                 throw new IllegalStateException("Unknown Build.DEVICE " + Build.DEVICE);
         }
